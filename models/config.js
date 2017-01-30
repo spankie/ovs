f25dd088d9de5908984aa8ba053970f90636ff79
spankie <odohi.david@gmail.com>
2017-01-30 09:08:03 +0100

    added a remote database config

--- models/config.js
+++ models/config.js
@@ -1,9 +1,10 @@
 var config = { 
   dbconf : {
-            host : 'localhost',
-            user : 'root',
-            password : 'toor',
-            database : 'vs'
+            // host : 'localhost',
+            host : '169.255.59.4',
+            user : 'edbecker_ovs',
+            password : 'Admin123',
+            database : 'edbecker_ovs'
         }
 };
 
